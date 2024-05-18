e1a2841637a2d133b56da861674cdff3add11106
hokto <hokuto.k1441@gmail.com>
2024-05-18 20:53:18 +0900

    python用テンプレを作成

---
+++ mylib/template.py
@@ -0,0 +1,20 @@
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10 ** 6)
+
+def myin():
+    return stdin.readline().rstrip()
+
+def myin_sp():
+    return stdin.readline().rstrip().split()
+
+def myin_sp_i():
+    return map(int,myin_sp())
+
+def myin_sp_s():
+    return map(str,myin_sp())
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
