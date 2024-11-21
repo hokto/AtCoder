51aa60760587c15fc9e68885e41cebadad411b52
hokto <hokuto.k1441@gmail.com>
2024-11-21 19:48:16 +0900

    abc367のf解いた

--- ABC/abc367/f/f.py
+++ ABC/abc367/f/f.py
@@ -17,7 +17,32 @@ def myin_sp_s():
     return list(map(str,myin_sp()))
 
 def main():
-    pass
+    import random
+    MOD = 1<<61-1
+    N,Q = myin_sp_i()
+    A = myin_sp_i()
+    B = myin_sp_i()
+    hash_table = {}
+    for a in A:
+        hash_table[a] = random.randrange(MOD)
+    for b in B:
+        hash_table[b] = random.randrange(MOD)
+    accum_xor_A = [0]*(N+1)
+    accum_xor_B = [0]*(N+1)
+    for i in range(N):
+        accum_xor_A[i+1] = (accum_xor_A[i]+hash_table[A[i]])%MOD
+        accum_xor_B[i+1] = (accum_xor_B[i]+hash_table[B[i]])%MOD
+        
+    for q in range(Q):
+        l,r,L,R = myin_sp_i()
+        l-=1
+        r-=1
+        L-=1
+        R-=1
+        if (accum_xor_A[r+1]-accum_xor_A[l])%MOD==(accum_xor_B[R+1]-accum_xor_B[L])%MOD:
+            print("Yes")
+        else:
+            print("No")
 
 if __name__ == "__main__":
     main()
