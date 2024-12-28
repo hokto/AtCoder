3b4af9510b53f80a97a6a204cc622eef921a9c2f
hokto <hokuto.k1441@gmail.com>
2024-12-28 19:15:29 +0900

    abc310のd解いた

--- ABC/abc310/d/d.py
+++ ABC/abc310/d/d.py
@@ -1,8 +1,8 @@
 from sys import stdin,setrecursionlimit
 setrecursionlimit(10 ** 6)
 # 再帰用
-#import pypyjit
-#pypyjit.set_param('max_unroll_recursion=-1')
+import pypyjit
+pypyjit.set_param('max_unroll_recursion=-1')
 
 def myin():
     return stdin.readline().rstrip()
@@ -17,41 +17,34 @@ def myin_sp_s():
     return list(map(str,myin_sp()))
 
 def main():
-    import math
     N,T,M = myin_sp_i()
-    E = [[] for i in range(N)]
-    for i in range(M):
+    hate = [0]*N
+    for _ in range(M):
         a,b = myin_sp_i()
         a-=1
         b-=1
-        E[a].append(b)
-    vio_select = [[0]*N for i in range(N)]
-    team_size = [0]*T
+        hate[a]|=1<<b
+        hate[b]|=1<<a
+    
+    teams = []
     ans = 0
-    def dfs(v,i,team_cnt):
-        nonlocal ans
-        if T-team_cnt>N-v:
+    def dfs(now):
+        nonlocal ans,teams
+        if now==N:
+            if len(teams)==T:
+                ans+=1
             return
-        if v==N:
-            ans+=1
-            return
-        if v!=0:
-            for vv in E[v-1]:
-                vio_select[vv][i] +=1
-        for j in range(T):
-            if vio_select[v][j]>0: continue
-            team_size[j]+=1
-            if team_size[j]==1:
-                dfs(v+1,j,team_cnt+1)
-            else:
-                dfs(v+1,j,team_cnt)
-            team_size[j]-=1
-        if v!=0:
-            for vv in E[v-1]:
-                vio_select[vv][i] -=1
-        
-    dfs(0,0,0)
-    print(ans//math.factorial(T))
+        for i in range(len(teams)):
+            if (teams[i] & hate[now])==0:
+                teams[i]^=1<<now
+                dfs(now+1)
+                teams[i]^=1<<now
+        if len(teams)<T:
+            teams.append(1<<now)
+            dfs(now+1)
+            teams.pop()
+    dfs(0)
+    print(ans)
 
 if __name__ == "__main__":
     main()
