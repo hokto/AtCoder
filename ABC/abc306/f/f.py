b3428c4454efe07b62d4ee1e9b87e674672802f8
hokto <hokuto.k1441@gmail.com>
2024-12-28 16:56:03 +0900

    abc306のf解いた

--- ABC/abc306/f/f.py
+++ ABC/abc306/f/f.py
@@ -17,7 +17,36 @@ def myin_sp_s():
     return list(map(str,myin_sp()))
 
 def main():
-    pass
+    from atcoder.fenwicktree import FenwickTree
+    # 全体でi番目の要素が集合A_idxにあることがわかって，かつA_idxの中でj番目であることがわかれば，
+    # idx~N-1までの集合に対して，j番目以上になり，どれほどその番数が上がるかどうかは結局合計して残りの分だけになる．
+    # 今，i番目でしかもA_idxにはすでにj個あることがわかっているため，残りはi-jであることがわかる．
+    # よって，答えに寄与する値は(N-1-idx)*(j+1)+i-jと計算できる(ただし，i,jは0-indexed)
+    N,M = myin_sp_i()
+    A = []
+    B = []
+    for _ in range(N):
+        A.append(myin_sp_i())
+        for a in A[-1]:
+            B.append(a)
+    change = {}
+    for i,b in enumerate(sorted(B)):
+        change[b] = i
+    idx_of_set = [-1]*(N*M) # idx_of_set[a]=iならA[i]にaが含まれている
+    for i in range(N):
+        for j in range(M):
+            A[i][j] = change[A[i][j]]
+            idx_of_set[A[i][j]] = i
+    ft = FenwickTree(N)
+    ans = 0
+    for i in range(N*M):
+        idx = idx_of_set[i]
+        resi = i-ft.sum(0,idx+1)
+        ans+=resi+(ft.sum(idx,idx+1)+1)*(N-1-idx)
+        #print(i,":",resi+(ft.sum(idx,idx+1)+1)*(N-1-idx))
+        ft.add(idx,1)
+    print(ans)
+    
 
 if __name__ == "__main__":
     main()
