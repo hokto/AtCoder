c13cb7c0536b8706996c8d35b4cf3836dbd5343e
hokto <hokuto.k1441@gmail.com>
2025-01-02 16:55:04 +0900

    abc359のd解いた

--- ABC/abc359/d/d.py
+++ ABC/abc359/d/d.py
@@ -16,18 +16,54 @@ def myin_sp_s():
     return list(map(str,myin_sp()))
 
 def main():
+    MOD = 998244353
     N,K = myin_sp_i()
     S = myin()
-    dp = [[0]*2 for i in range(N+1)]
-    dp[0][0] = dp[0][1]=1
+    dp = [[0]*(1<<K) for i in range(N+1)] # dp[i][j]:=i番目までで直前の長さKの集合がjとなる文字列の個数(a=0,b=1とする)
+    dp[0][0] = 1 # 一方からのみ遷移できるように片方だけ1
     for i in range(N):
-        is_parind = True
-        if i<K:
-            is_parind = False
-        else:
-            for k in range(i-K,i-K+K//2):
-                pass
-            
+        for j in range(1<<K):
+            if i<K-1:
+                if S[i]!="B":
+                    dp[i+1][j]+=dp[i][j]
+                    dp[i+1][j]%=MOD
+                if S[i]!="A":
+                    dp[i+1][j|(1<<i)]+=dp[i][j]
+                    dp[i+1][j|(1<<i)]%=MOD
+            elif i==K-1:
+                if S[i]!="B":
+                    bit = j
+                    bit1 = format(bit,"b").zfill(K)[:K//2] # 上位K//2bitを取得
+                    bit2 = "".join(reversed(format(bit,"b").zfill(K)[(K+1)//2:K])) # 下位K//2bitを取得
+                    if bit1!=bit2:
+                        dp[i+1][bit]+=dp[i][j]
+                        dp[i+1][bit]%=MOD
+                    
+                if S[i]!="A":
+                    bit = j|(1<<(K-1))
+                    bit1 = format(bit,"b").zfill(K)[:K//2] # 上位K//2bitを取得
+                    bit2 = "".join(reversed(format(bit,"b").zfill(K)[(K+1)//2:K])) # 下位K//2bitを取得
+                    if bit1!=bit2:
+                        dp[i+1][bit]+=dp[i][j]
+                        dp[i+1][bit]%=MOD
+            else:
+                if S[i]!="B":
+                    bit = j>>1
+                    bit1 = format(bit,"b").zfill(K)[:K//2] # 上位K//2bitを取得
+                    bit2 = "".join(reversed(format(bit,"b").zfill(K)[(K+1)//2:K])) # 下位K//2bitを取得
+                    if bit1!=bit2:
+                        dp[i+1][bit]+=dp[i][j]
+                        dp[i+1][bit]%=MOD
+                    
+                if S[i]!="A":
+                    bit = (j>>1)|(1<<(K-1))
+                    bit1 = format(bit,"b").zfill(K)[:K//2] # 上位K//2bitを取得
+                    bit2 = "".join(reversed(format(bit,"b").zfill(K)[(K+1)//2:K])) # 下位K//2bitを取得
+                    if bit1!=bit2:
+                        dp[i+1][bit]+=dp[i][j]
+                        dp[i+1][bit]%=MOD
+    #print(dp)
+    print(sum(dp[-1])%MOD)
             
 if __name__ == "__main__":
     main()
